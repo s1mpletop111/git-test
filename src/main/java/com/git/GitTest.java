be58a0427bb8caab066c188566903b2ad858099b
s1mpletop111 <100059284+s1mpletop111@users.noreply.github.com>
2023-08-04 16:42:25 +0800

    Update GitTest.java

--- src/main/java/com/git/GitTest.java
+++ src/main/java/com/git/GitTest.java
@@ -14,5 +14,6 @@ public class GitTest {
         System.out.println("hotfix");
         System.out.println("push test");
         System.out.println("push test1");
+         System.out.println("pull test1");
     }
 }

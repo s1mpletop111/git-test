89bf67dbe1cda504a310b1a86dcd5ebc094a8fe0
xjx <1325884575@qq.com>
2023-08-04 15:56:16 +0800

    third commit

--- src/main/java/com/git/GitTest.java
+++ src/main/java/com/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("helloGit");
         System.out.println("helloGit2");
+        System.out.println("helloGit3");
     }
 }
